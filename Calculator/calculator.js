de53936a892e5cad8b46e36091994381d8bd82fc
Chandrima <chandrimapal2001@gmail.com>
2023-10-28 20:15:04 +0530

    Final changes done

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -17,11 +17,11 @@ function toggleHistory() {
 }
 
 
+
 function deleteLastCharacter() {
     const input = document.getElementById('input').value;
     document.getElementById('input').value = input.slice(0, -1);
 }
-
 
 function appendToInput(value) {
     document.getElementById('input').value += value;
@@ -39,7 +39,33 @@ function clearHistory() {
 function calculateResult() {
     try {
         const input = document.getElementById('input').value;
-        const result = eval(input);
+        let result;
+
+        if (input.includes('%')) {
+            const numArray = input.split('*');
+            if (numArray.length === 2) {
+                const num1 = parseFloat(numArray[0].trim());
+                const num2 = parseFloat(numArray[1].trim());
+                result = (num1 * num2) / 100;
+            } else {
+                throw new Error('Invalid input');
+            }
+        } else if (input.includes('√')) {
+            // Handle square root calculation
+            const num = parseFloat(input.substring(1));
+            if (!isNaN(num)) {
+                result = Math.sqrt(num);
+            } else {
+                throw new Error('Invalid input');
+            }
+        } else if (input.includes('^')) {
+            // Handle squaring
+            const num = parseFloat(input);
+            result = num * num;
+        } else {
+            // Evaluate the expression
+            result = eval(input);
+        }
 
         history += `${input} = ${result}\n`;
         document.getElementById('history').value = history;
